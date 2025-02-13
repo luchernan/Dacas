d9424422b701e062132aa111c98473595deee2bb
luchernan <lukihernandez2005@gmail.com>
2025-02-13 12:34:17 +0100

    routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,18 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig,
+layout,
+route,
+index,
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+ } from "@react-router/dev/routes";
+
+
+  export default [
+    layout("routes/home.tsx", [
+      index("routes/home.tsx"), // Home page (acts as the layout)
+      route("about", "routes/about.tsx"), // About as a child route
+      route("contact", "routes/search.tsx"), // Search as a child route
+      route("services", "routes/details.tsx"), // Details as a child route
+    ]),
+  ] satisfies RouteConfig;
+  
+
